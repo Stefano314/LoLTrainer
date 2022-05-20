f8fe29ce11d14e09680921eb1c7ac243c44ca898
stefano <stefanobianchi314@gmail.com>
2022-05-20 11:43:58 +0200

    Major update. Items now are a class that stores all the interesting stats of that item. This will allow an easier evaluation of the effects of an item on a champion.

--- LoLTrainer/Items/items.py
+++ LoLTrainer/Items/items.py
@@ -1,12 +1,13 @@
 import json
 import os
 
-__all__ = ['get_item']
+__all__ = ['LoLItems', 'get_items']
 
 INFO_PATH = os.path.join(os.getcwd(), 'LoLTrainer', 'Items', '')
 
 # Cache global variable for .json
 _cache = dict()
+
 
 def clear_cache(key):
     """
@@ -19,7 +20,8 @@ def clear_cache(key):
     if key in _cache:
         _cache.pop(key)
 
-def load_item(filename : str ='items'):
+
+def _load_item(filename : str = 'items'):
     """
     Description
     -----------
@@ -36,7 +38,7 @@ def load_item(filename : str ='items'):
     return _cache[key]
 
 
-def get_item(keys : list, filename :str = 'items'):
+def _get_item(keys : str, filename : str = 'items'):
     """
     Description
     -----------
@@ -45,14 +47,69 @@ def get_item(keys : list, filename :str = 'items'):
 
     Parameters
     ----------
-    keys : int
-        Selects the entry in the .json file, which correspond to a specific champion.
-    file_name :str
+    keys : str
+        Selects the entry in the .json file, which correspond to a single specific item.
+    filename :str
+        Name of the .json file. At the moment we only have 'items.json'.
+
+    Return
+    ------
+    dict : The items dictionary that will be used in items class to generate the object.
+
     """
 
-    items = load_item(filename)
-    # !! put controls if items exits !!
-    if len([keys]) == 1:
-        return items.get(keys)
+    items = _load_item(filename)
+    return items.get(keys)
+
+
+def get_items(keys : list):
+
+    """
+    Description
+    -----------
+    Create LoLItems objects according to the names specified in 'keys'.
+
+    Parameters
+    ----------
+    keys : list
+        Objects names.
+
+    """
+
+    if isinstance(keys, str):
+        return LoLItems(name = keys)
+
     else:
-        return [items.get(key) for key in keys]
+        return [LoLItems(name = key) for key in keys]
+
+
+class LoLItems:
+
+    '''
+    Description
+    -----------
+    League of Legends items class.
+    This class will serve the champion class when evaluating stats after the acquisition of an item.
+    Because of that, the structure is actually the same of the champion class, with the addition of few features.
+
+    '''
+
+    def __init__(self, name: str):
+        # PRIVATE
+        self._items_stats = _get_item(filename = 'items', keys = name)
+
+        # PUBLIC:
+        self.name = self._items_stats['name']
+        self.gold = self._items_stats['gold']
+        self.HP = self._items_stats['HP']
+        self.AP = self._items_stats['AP']
+        self.AD = self._items_stats['AD']
+        self.Ph_armor = self._items_stats['physical_armor']
+        self.Ma_armor = self._items_stats['magical_armor']
+        self.mana = self._items_stats['mana']
+        self.lethality = self._items_stats['lethality']
+        self.armor_penetration = self._items_stats['armor_penetration']
+        self.omivamp = self._items_stats['omivamp']
+        self.life_steal = self._items_stats['life_steal']
+        self.bonus = self._items_stats['BONUS']
+
