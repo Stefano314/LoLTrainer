162181ca8f8d36a49abc971595bc37f16590ab84
stefano <stefanobianchi314@gmail.com>
2022-05-22 17:59:06 +0200

    Created processing.py. I managed to recognize some images acquired with a screenshot, but still it needs some work.

--- LoLTrainer/Trainer/processing.py
+++ LoLTrainer/Trainer/processing.py
@@ -1,5 +1,129 @@
+import mss
+import numpy as np
+from PIL import Image
+import os
 
-def check_items() -> list:
-    # This function will probably be the unique source of
-    # in-game items getter, since we have to check the screen anyway
-    return [] # List of items hold
+# import matplotlib
+# import matplotlib.pyplot as plt
+
+ITEMS_IMAGES_PATH = os.path.join(os.getcwd(), 'Images', 'Items', '')
+
+__all__ = ['image_comparison', 'item_recognizer', 'get_hold_items']
+
+def _screen_acquisition():
+
+    # Acquire all screen
+    with mss.mss() as sct:
+
+        # 1 monitor FullHD
+        monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
+
+        screen_array = np.array(sct.grab(monitor))
+        screen_array = Image.fromarray(screen_array).convert('L')
+        screen_array = np.array(screen_array)
+
+    return screen_array
+
+
+def get_hold_items(screen: np.array) -> list:
+    """
+    Description
+    -----------
+    Subdivide the portion of the screen into 6 bunches, where items are hold in game by the champion.
+
+    Parameters
+    ----------
+    screen : numpy.array
+        Array containing the grayscale values of the whole screen.
+
+    Return
+    ------
+    list : List of numpy.array objects relative to the items spots in game.
+
+    """
+
+    x_step = [49 * i for i in range(0, 3)]  # Three item columns
+    y_step = [47 * i for i in range(0, 2)]  # Two item rows
+
+    # At the moment, all the position values are found empirically
+    return [screen[947 + y:987 + y, 1131 + x:1171 + x] for y in y_step for x in x_step]
+
+
+def image_comparison(img_items_list : list) -> list:
+    """
+    Description
+    -----------
+    Perform an image recognition and get the name of the object , if it exists.
+
+    Parameters
+    ----------
+    img_items_list : list
+        List containing the array bunches obtained from '_get_hold_items()' function.
+
+    Return
+    ------
+    list : List with the items names found, if they exist.
+
+    """
+
+    result = []
+    images = [img for img in os.listdir(ITEMS_IMAGES_PATH) if img.endswith('.png')]
+
+    try:
+
+        # Compare each bunch ...
+        for img_array in img_items_list:
+            # Threshold
+
+            img_array = np.where(img_array >= 90, 255, 0)
+            # plt.matshow(img_array, cmap = matplotlib.cm.Greys_r)
+            # plt.show()
+
+            # ... with every default image
+            for img_name in images:
+
+                # Convert to numpy grayscale
+                test = np.array(Image.open(os.path.join(ITEMS_IMAGES_PATH, img_name)).convert("L"))
+                if test.size == 1600:
+
+                    # Get number of equal pixels
+                    test = np.where(test >= 100, 255, 0)
+                    missed = np.abs(test - img_array)
+                    score = np.count_nonzero(missed) / img_array.size
+
+                    # Set threshold (lower means similar, since the difference of two
+                    # identical images is zero.
+                    # WE STILL HAVE TO SAY WHAT HAPPENS IF 2 (or more) IMAGES ARE FOUND!!
+                    if score < 0.05:
+                        result.append(img_name[:-4])
+    except:
+        pass
+
+    if len(result) == 0:
+        result = [None] * 6
+
+    return result
+
+
+def item_recognizer() -> list:
+    """
+    Description
+    -----------
+    Get the name of each object hold by the champion in game, by confronting the images with the default ones.
+
+    Return
+    ------
+    list : List of strings with the items names.
+
+    """
+
+    # Analyze bunches: get screenshot, divide it in bunches and perform the comparison.
+    result = image_comparison(get_hold_items(_screen_acquisition()))
+    if np.any(result):
+        # Get objects name (always a list)
+        return result[result.nonzero()[0]]
+
+    else:
+        print("- No Items found.")
+        return [None]*6
+
