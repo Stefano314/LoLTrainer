48f0f1ee2e474c5eb115cb6fcebece612bcbe9d3
stefano <stefanobianchi314@gmail.com>
2022-05-30 22:50:34 +0200

    Json files replaced with csv.

---
+++ Utility/champions_stats_template.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+def generate_champions_stats_template():
+    """
+    Generates all the csv files containing the information related to every champion.
+
+    """
+
+    champs = ['Aatrox', 'Ahri', 'Akali', 'Akshan', 'Alistar', 'Amumu', 'Anivia', 'Annie',
+              'Aphelios', 'Ashe', 'Aurelion Sol', 'Azir', 'Bard', 'Blitzcrank', 'Brand', 'Braum',
+              'Caitlyn', 'Camille', 'Cassiopeia', "Cho'Gath", 'Corki', 'Darius', 'Diana', 'Dr. Mundo',
+              'Draven', 'Ekko', 'Elise', 'Evelynn', 'Ezreal', 'Fiddlesticks', 'Fiora', 'Fiora', 'Fizz',
+              'Galio', 'Gangplank', 'Garen', 'Gnar', 'Gragas', 'Graves', 'Gwen', 'Hecarim', 'Heimerdinger',
+              'Illaoi', 'Irelia', 'Ivern', 'Janna', 'Jarvan IV', 'Jax', 'Jayce', 'Jhin', 'Jinx', "Kai'Sa",
+              'Kalista', 'Karma', 'Karthus', 'Kassadin', 'Katarina', 'Kayle', 'Kayn', 'Kennen', "Kha'Zix",
+              'Kindred', 'Kled', "Kog'Maw", 'LeBlanc', 'Lee Sin', 'Leona', 'Lillia', 'Lissandra', 'Lucian',
+              'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai', 'Master Yi', 'Miss Fortune', 'Mordekaiser',
+              'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Neeko', 'Nidalee', 'Nocturne', 'Nunu & Willump',
+              'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus',
+              "Rek'Sai", 'Rell', 'Renata Glasc', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Samira',
+              'Sejuani', 'Senna', 'Seraphine', 'Sett', 'Shaco', 'Shen', 'Shyvana', 'Singed', 'Sion', 'Sivir',
+              'Skarner', 'Sona', 'Soraka', 'Swain', 'Sylas', 'Syndra', 'Tahm Kench', 'Taliyah', 'Talon',
+              'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'Twisted Fate', 'Twitch',
+              'Udyr', 'Urgot', 'Varus', 'Vayne', 'Veigar', "Vel'Koz", 'Vex', 'Vi', 'Viego', 'Viktor', 'Vladimir',
+              'Volibear', 'Warwick', 'Wukong', 'Xayah', 'Xerath', 'Xin Zhao', 'Yasuo', 'Yone', 'Yorick',
+              'Yuumi', 'Zac', 'Zed', 'Zeri', 'Zeri', 'Zilean', 'Zoe', 'Zyra']
+
+    def_stats = ["HP", "HP_growth", "mana", "mana_growth", "HP_regen", "HP_regen_growth", "mana_regen",
+                 "mana_regen_growth", "physical_armor", "magical_armor", "tenacity"]
+    off_stats = ["AD", "AP", "att_speed", "att_range", "AD_growth", "mov_speed", "crit_dmg", "crit_%", "AH"]
+    generalities = ["nickname", "resource_type", "class", "position", "range_type"  "latest_patch"]
+
+    pd.DataFrame(0, index = def_stats, columns = champs).to_csv('champions_defensive_template.csv')
+    pd.DataFrame(0, index = off_stats, columns = champs).to_csv('champions_offensive_template.csv')
+    pd.DataFrame(0, index = generalities, columns = champs).to_csv('generalities_template.csv')
