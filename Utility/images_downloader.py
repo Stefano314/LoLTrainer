457b70a328cc55aedb78437706524edd81cbde8a
stefano <stefanobianchi314@gmail.com>
2022-06-20 09:55:45 +0200

    Improved images_downloader.py.

--- Utility/images_downloader.py
+++ Utility/images_downloader.py
@@ -9,31 +9,25 @@ def get_item_images():
 
     images_links = [i for i in site_content if i.startswith("https://static.wikia.nocookie")]
     images_names = [i.split('/') for i in images_links]
+
     print("- Downloading items images from 'leagueoflegends.fandom.com' ...")
 
     # Save items names in list
-    with open(ITEM_IMAGES_PATH + 'items_names.txt', 'w') as f1:
 
-        f1.write('[')
+    for i in range(len(images_links)):
 
-        for i in range(len(images_links)):
+        try:
+            pos = images_names[i].index("revision")
+            img_data = requests.get(images_links[i]).content # Actual image
 
-            try:
-                pos = images_names[i].index("revision")
-                img_data = requests.get(images_links[i]).content # Actual image
+            # Process names
+            img_name = images_names[i][pos - 1].replace("%27", "\'").replace("%28", "(").replace("%29", ")").replace("%21", "!").replace("_item", '').replace("_", " ")
 
-                # Save names
-                img_name = images_names[i][pos - 1].replace("%27", "\'").replace("_item", '').replace("_", " ")
-                f1.write(f'"{img_name}"')
-                f1.write(", ")
+            # Save images
+            with open(f'{ITEM_IMAGES_PATH+img_name}', 'wb') as img:
+                img.write(img_data)
 
-                # Save images
-                with open(f'{ITEM_IMAGES_PATH+img_name}', 'wb') as f2:
-                    f2.write(img_data)
-
-            except:
-                pass
-
-        f1.write(']')
+        except:
+            pass
 
     print(f"Completed!\nImages saved in {ITEM_IMAGES_PATH}")
